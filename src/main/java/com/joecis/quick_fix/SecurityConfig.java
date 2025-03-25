35273800e3b71fee3cdbdf2538dbc1a4dc99bb62
JoseCisCh <andres.cis.chau@gmail.com>
2025-03-25 17:57:05 -0600

    Addition of new query method in UserRepository to fetch users by username.
    Creation of files:
    
        - SecurityConfig: Setting base configuration for security in
          application.
        - AppUserDetailsServies: Custom implementation of UserDetailsService
          required to use our custom UserDetails implementation class (User).

---
+++ src/main/java/com/joecis/quick_fix/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.joecis.quick_fix;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.joecis.quick_fix.user.User;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults())
+            .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    } 
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        AuthenticationManager authenticationManager;
+        try {
+            authenticationManager = authenticationConfiguration.getAuthenticationManager();
+        } catch(Exception e) {
+            throw new Exception("Could not obtain authentication manager");
+        }
+
+        return authenticationManager;
+    }
+
+}
---
+++ src/main/java/com/joecis/quick_fix/user/AppUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.joecis.quick_fix.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+    UserRepository userRepository;
+
+    public AppUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User with username " + username + " not found.");
+        }
+
+        return user;
+    }
+
+}
